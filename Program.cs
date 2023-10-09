7eada3b1a6d9c085cc21922e803c51c995cb8c3c
Hasselgren Marcus <s2300288@BC-C02GR0BDQ6LR-MacBook-Air.local>
2023-10-09 12:59:13 +0300

    initial3

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Faker;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            var nimi = Name.First();
+            var sukunimi = Name.Last();
+            var osoite = Address.StreetAddress(true);
+            var henkilotunnus = GeneroiHenkilotunnus();
+
+            Console.WriteLine($"Nimi ja Sukunimi: {nimi} {sukunimi}");
+            Console.WriteLine($"Osoite: {osoite}");
+            Console.WriteLine($"Henkilötunnus: {henkilotunnus}");
+            Console.WriteLine("------------------------------------");
+        }
+    }
+
+
+    private static string GeneroiHenkilotunnus()
+    {
+        var random = new Random();
+        var kirjain = (char)random.Next('A', 'Z' + 1);
+        var numerot = $"{random.Next(1000, 9999)} {random.Next(1000, 9999)}";
+        return $"{kirjain} {numerot}";
+    }
+}
+
